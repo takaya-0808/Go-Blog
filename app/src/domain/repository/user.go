9ff1f0aa6a9ecae0858e4bc5f789c1040933e02b
takaya <takaya1912@gmail.com>
2021-08-17 01:53:20 +0900

    add repository

--- app/src/domain/repository/user.go
+++ app/src/domain/repository/user.go
@@ -1,10 +1,10 @@
 package repository
 
-import 
+import "Go-Blog/app/src/domain/model"
 
-type Usermodel interface {
-	Get(int) (*model.UserModel, error)
+type UserRepository interface {
+	Get(name string) (*model.UserModel, error)
 	GetAll() []model.UserModel
-	Delte(id int) error
-	Add(user *model.UserModel) (id int, err error)
+	// Delte(id int) error
+	// Add(user *model.UserModel) (id int, err error)
 }
