62e946fcec63cd3c6c6ea872fcce1b02cc70fa34
takaya <takaya1912@gmail.com>
2021-09-10 00:27:11 +0900

    fix Add function

--- app/src/domain/repository/user.go
+++ app/src/domain/repository/user.go
@@ -5,10 +5,8 @@ import "Go-Blog/app/src/domain/model"
 type UserRepository interface {
 	Get(name string) (*model.UserModel, error)
 	GetAll() []model.UserModel
-	// Delte(id int) error
-	// Add(user *model.UserModel) (id int, err error)
-	RegisterCheck(name string, email string) error
-	Add(user model.RegisterModel) error
+	Check(name string, email string) error
+	Add(name string, email string, pass string) error
 }
 
 type BlogRepository interface{}
