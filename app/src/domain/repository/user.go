1bf29accdbdf76729ac3c0c2ea000e5435effa75
takaya <takaya1912@gmail.com>
2021-09-10 23:15:02 +0900

    add

--- app/src/domain/repository/user.go
+++ app/src/domain/repository/user.go
@@ -5,7 +5,7 @@ import "Go-Blog/app/src/domain/model"
 type UserRepository interface {
 	Get(name string) (*model.UserModel, error)
 	GetAll() []model.UserModel
-	Check(name string, email string) error
+	FindByName(name string) (string, error)
 	Add(name string, email string, pass string) error
 }
 
