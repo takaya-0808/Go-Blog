0a2b96a157d72d9a9971b7989e207bdc4091106b
takaya <takaya1912@gmail.com>
2021-09-19 01:42:55 +0900

    add router

---
+++ app/src/infrastructure/router/router.go
@@ -0,0 +1,33 @@
+package router
+
+import (
+	"Go-Blog/app/config"
+	"Go-Blog/app/src/infrastructure/database"
+	"Go-Blog/app/src/interfaces/handler"
+	"Go-Blog/app/src/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouting() *gin.Engine {
+
+	router := gin.Default()
+	userDatabases := database.NewUserDabase(config.Connection())
+	userUseCases := usecase.NewUseUsecase(userDatabases)
+	userHandler := handler.NewUserHandler(userUseCases)
+
+	routerEngin := router.Group("/MyBlog")
+	{
+		v1 := routerEngin.Group("/api/v1")
+		{
+			v1.GET("/", userHandler.Show)
+			v1.GET("/:name", userHandler.Index)
+		}
+		auth := routerEngin.Group("/api/auth")
+		{
+			auth.POST("/login", userHandler.Login)
+			auth.POST("/register", userHandler.Register)
+		}
+	}
+	return router
+}
--- main.go
+++ main.go
@@ -1,32 +1,11 @@
 package main
 
 import (
-	"Go-Blog/app/config"
-	"Go-Blog/app/src/infrastructure/database"
-	"Go-Blog/app/src/interfaces/handler"
-	"Go-Blog/app/src/usecase"
-
-	"github.com/gin-gonic/gin"
+	"Go-Blog/app/src/infrastructure/router"
 )
 
 func main() {
 
-	userDatabases := database.NewUserDabase(config.Connection())
-	userUseCases := usecase.NewUseUsecase(userDatabases)
-	userHandler := handler.NewUserHandler(userUseCases)
-
-	router := gin.Default()
-	routerEngin := router.Group("/MyBlog")
-	{
-		v1 := routerEngin.Group("/api/v1")
-		{
-			v1.POST("/register", userHandler.Register)
-			v1.POST("/login", userHandler.Login)
-			v1.GET("/:name", userHandler.Index)
-			v1.GET("/", userHandler.Show)
-		}
-
-	}
-
-	router.Run(":8022")
+	r := router.InitRouting()
+	r.Run(":8022")
 }
