edb957a7aa245faca4f60a4f1cc0f40d08541bfe
takaya <takaya1912@gmail.com>
2021-09-30 11:53:16 +0900

    add Article struct

--- app/src/domain/model/Blog.go
+++ app/src/domain/model/Blog.go
@@ -1,7 +1,12 @@
 package model
 
-type PostArticle struct {
-	id      int    `json:"Id"`
-	title   string `json:"Title"`
-	context string `json:"Context"`
+import "time"
+
+type Article struct {
+	ID        int64     `json:"id"`
+	Title     string    `json:"title" validate:"required"`
+	Content   string    `json:"content" validate:"required"`
+	Author    string    `json:"author"`
+	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at"`
 }
