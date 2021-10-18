4de99309b6b8c7ee9922707ad323e927af25413b
takaya <takaya1912@gmail.com>
2021-10-18 23:30:01 +0900

    add test code

---
+++ app/src/interfaces/handler/user_test.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"Go-Blog/app/src/infrastructure/router"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRegister(t *testing.T) {
+
+	router := router.InitRouting()
+	w := httptest.NewRecorder()
+	// name := "hoge"
+	// email := "xxx@xxx.ac.jp"
+	// pass := "password"
+	// user := &model.RegisterModel{
+	// 	UserName: name,
+	// 	UserEmail: email,
+	// 	UserPassWord: pass,
+	// }
+	req, _ := http.NewRequest("GET", "/MyBlog//api/v1/", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+
+}
