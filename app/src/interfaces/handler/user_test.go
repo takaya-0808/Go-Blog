4656c69cf886a1b8263d2b16bd18e3b740639e91
takaya <takaya1912@gmail.com>
2021-10-21 01:32:29 +0900

    rmv comment out

--- app/src/interfaces/handler/user_test.go
+++ app/src/interfaces/handler/user_test.go
@@ -14,14 +14,7 @@ func TestShow(t *testing.T) {
 
 	router := router.InitRouting()
 	w := httptest.NewRecorder()
-	// name := "hoge"
-	// email := "xxx@xxx.ac.jp"
-	// pass := "password"
-	// user := &model.RegisterModel{
-	// 	UserName: name,
-	// 	UserEmail: email,
-	// 	UserPassWord: pass,
-	// }
+
 	req, _ := http.NewRequest("GET", "/MyBlog/api/v1/", nil)
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
@@ -30,5 +23,7 @@ func TestShow(t *testing.T) {
 }
 
 func TestIndex(t *testing.T) {
+	router := router.InitRouting()
+	w := httptest.NewRecorder()
 
 }
