1a61036180d886aaffe9c686ecf38cd20b95ed47
takaya <takaya1912@gmail.com>
2021-11-12 18:19:54 +0900

    add

--- app/src/interfaces/handler/blog.go
+++ app/src/interfaces/handler/blog.go
@@ -1,1 +1,13 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// var secretKey := os.Getenv("SECRETKEY")
+
+type BlogHandler interface{}
+
+type blogHandler struct{}
+
+func (bh blogHandler) Show(c *gin.Context) {}
