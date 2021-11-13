c9718b38e57a8ce37dd0ccce1445edd6f78eb21a
takaya <takaya1912@gmail.com>
2021-11-13 11:17:36 +0900

    fix blog.sql

--- database/initdb.d/blog.sql
+++ database/initdb.d/blog.sql
@@ -1,6 +1,13 @@
--- DROP TABLE IF EXISTS `blog`;
+DROP TABLE IF EXISTS `blogs`;
 
--- create table IF not exists `users` (
---     id INT NOT NULL AUTO_INCREMENT,
---     author 
--- )
+create table IF not exists `blogs` (
+    id INT NOT NULL AUTO_INCREMENT,
+    author VARCHAR(32) NOT NULL,
+    title VARCHAR(32) NOT NULL,
+    context VARCHAR(1024) NOT NULL,
+    created_at datetime  default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO blogs (id,author,title,context) VALUES (1, 'TOM','xxxx@mail.co.jp', 'hogehoge');
