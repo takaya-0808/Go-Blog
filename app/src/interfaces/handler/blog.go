758574eac8e776e3c0c24af378eb17daacacccc4
takaya <takaya1912@gmail.com>
2021-12-08 17:26:52 +0900

    add

--- app/src/interfaces/handler/blog.go
+++ app/src/interfaces/handler/blog.go
@@ -1,13 +1,34 @@
 package handler
 
 import (
+	"Go-Blog/app/src/usecase"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 // var secretKey := os.Getenv("SECRETKEY")
 
-type BlogHandler interface{}
+type BlogHandler interface {
+	Show(c *gin.Context)
+}
 
-type blogHandler struct{}
+type blogHandler struct {
+	blogUsecase usecase.BlogUseCase
+}
 
-func (bh blogHandler) Show(c *gin.Context) {}
+func NewBlogHandler(bu usecase.BlogUseCase) BlogHandler {
+	return &blogHandler{
+		blogUsecase: bu,
+	}
+}
+
+func (bh blogHandler) Show(c *gin.Context) {
+
+	artcles, err := bh.blogUsecase.Index()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "error"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "OK", "blog": artcles})
+}
