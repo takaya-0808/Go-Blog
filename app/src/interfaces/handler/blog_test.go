0295273af8ff2b9657e94156eeb45782c70c47f7
takaya <takaya1912@gmail.com>
2021-12-21 23:50:55 +0900

    add test

---
+++ app/src/interfaces/handler/blog_test.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"Go-Blog/app/src/infrastructure/router"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShow(t *testing.T) {
+
+	router := router.InitRouting()
+	w := httptest.NewRecorder()
+
+	req, _ := http.NewRequest("GET", "/Myblog/api/article/", nil)
+	router.ServeHTTP(w, req)
+	// assert.Equal(t, http.StatusOK, w.Code)
+	assert.NotEqual(t, http.StatusOK, w.Code)
+}
