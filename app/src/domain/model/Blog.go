ecc3edb9ac84d9e37ba011388e346e91484fa3e8
takaya <takaya1912@gmail.com>
2022-01-02 21:32:32 +0900

    add

--- app/src/domain/model/Blog.go
+++ app/src/domain/model/Blog.go
@@ -11,6 +11,12 @@ type Article struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
+type CreateArticle struct {
+	Title   string `json:"title" validate:"required"`
+	Content string `json:"content" validate:"required"`
+	Author  string `json:"author"`
+}
+
 type TitlesShow struct {
 	Title string `json:"title"`
 	URL   string `json:"url"`
