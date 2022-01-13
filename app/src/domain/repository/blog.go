6f34b1d850bf92c18ec35c5ff2972fdf1187cbba
takaya <takaya1912@gmail.com>
2022-01-14 00:19:40 +0900

    add interface titleShow

--- app/src/domain/repository/blog.go
+++ app/src/domain/repository/blog.go
@@ -5,5 +5,6 @@ import "Go-Blog/app/src/domain/model"
 type BlogRepository interface {
 	GetAllArticle() (*[]model.Article, error)
 	GetOneArticle(id int) (*model.Article, error)
+	TitleShow() (*[]model.TitlesShow, error)
 	PostArticle(*model.Article) error
 }
