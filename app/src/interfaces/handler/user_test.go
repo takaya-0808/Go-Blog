075581578265ceb52d48f014d18cdf0635f1533c
takaya <takaya1912@gmail.com>
2022-01-14 23:45:56 +0900

    add test code

--- app/src/interfaces/handler/user_test.go
+++ app/src/interfaces/handler/user_test.go
@@ -23,7 +23,7 @@ func TestShow(t *testing.T) {
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.NotEqual(t, http.StatusBadRequest, w.Code)
 	// assert.Equal(t, http.StatusBadRequest, w.Code)
-}
+} 
 
 func TestIndex(t *testing.T) {
 
