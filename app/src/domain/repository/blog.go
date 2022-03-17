a6824eb22369e8e3a51de80da4e00e68da2fff46
takaya <takaya1912@gmail.com>
2022-03-18 00:30:07 +0900

    add interface

--- app/src/domain/repository/blog.go
+++ app/src/domain/repository/blog.go
@@ -5,6 +5,7 @@ import "Go-Blog/app/src/domain/model"
 type BlogRepository interface {
 	GetAllArticle() (*[]model.Article, error)
 	GetOneArticle(id int) (*model.Article, error)
+	TitleOneShow(id int) (*model.TitlesShow, error)
 	TitleShow() (*[]model.TitlesShow, error)
 	PostArticle(article *model.CreateArticle) error
 }
